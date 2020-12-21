c5e698508ef4a172724d4cf0306efbf86e1bea10
Darlinson Alves <darlinsonalves@gmail.com>
2020-12-21 07:18:05 -0300

    bash para set de variáveis de ambiente...

---
+++ envs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 'SET URL_MONGO'
+export URL_MONGO='mongodb+srv://users:nH}DaipF0Cgw1SB)v@cluster0.7ok9z.mongodb.net/service-users?retryWrites=true&w=majority'
+
+echo 'SET PORT'
+export URL_PORT_API='3000'
+
+echo 'SET ENVS FACEBOOK APP'
+export FACEBOOK_APP_ID='150670660180166'
+export FACEBOOK_APP_SECRET='ae1f223e9478da1dd1cfa186509c1a52'
+export FACEBOOK_URL_CALLBACK='http://localhost:'$URL_PORT_API'/facebook/callback'
